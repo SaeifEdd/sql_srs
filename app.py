99173ad620010a3456c9ebf908a5ecf8080d2b9f
SaeifEdd <saeifchaabouni@gmail.com>
2024-03-08 16:03:58 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import duckdb as db
+st.write('hello world')
+data = {"a": [1, 2, 3], "b": [4, 5, 6]}
+df = pd.DataFrame(data)
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
+
+with tab1:
+    sql_query = st.text_area(label='write your sql command')
+    result = db.query(sql_query).df()
+    st.write(f"your sql command is: {sql_query}")
+    st.dataframe(result)
+
+with tab2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
