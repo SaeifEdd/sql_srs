b6edb2a75559120ec00b1a5f259dff11982c9152
SaeifEdd <saeifchaabouni@gmail.com>
2024-04-27 12:47:45 +0100

    adding database with exercises and memory state

--- app.py
+++ app.py
@@ -1,33 +1,13 @@
-import io
 import streamlit as st
-import pandas as pd
 import duckdb
 
-
-CSV = """
-beverage,price
-orange juice, 2.5
-expresso, 2
-Tea, 3
-citronade, 3.5
-"""
-beverages = pd.read_csv(io.StringIO(CSV))
-
-CSV2 = """
-food_item,food_prices
-cookie juice, 2.5
-chocolatine, 5
-muffin, 4
-cheese cake, 7
-"""
-
-food_items = pd.read_csv(io.StringIO(CSV2))
+con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
 ANSWER_STR = """
 SELECT * FROM beverages 
 CROSS JOIN food_items
 """
-solution_df = duckdb.sql(ANSWER_STR).df()
+#solution_df = duckdb.sql(ANSWER_STR).df()
 
 
 st.write(
@@ -38,42 +18,43 @@ st.write(
 )
 
 with st.sidebar:
-    option = st.selectbox(
+    theme = st.selectbox(
         "What would you like to review?",
-        ("Joins", "Group by", "Windows Functions"),
+        ("cross join", "Group by", "window_functions"),
         index=None,
         placeholder="Select contact method...",
     )
 
-    st.write("You selected:", option)
-
+    st.write("You selected:", theme)
+    exercise = con.execute(f"SELECT * FROM memory_state where theme = '{theme}' ").df()
+    st.write(exercise)
 
 st.header("enter your code")
 query = st.text_area(label="write your sql command", key="user input")
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
-
-    try:
-        result = result[solution_df.columns]
-        st.dataframe(result.compare(solution_df))
-    except KeyError as e:
-        st.write("There are some missing columns")
-
-    rows_difference = result.shape[0] - solution_df.shape[0]
-    if rows_difference != 0:
-        st.write(f"result has {rows_difference} rows different than solution")
-
-
-tab1, tab2 = st.tabs(["tables", "solution"])
-
-with tab1:
-    st.write("beverages")
-    st.dataframe(beverages)
-    st.write("foods")
-    st.dataframe(food_items)
-    st.write("expected")
-    st.dataframe(solution_df)
-
-with tab2:
-    st.write(ANSWER_STR)
+# if query:
+#     result = duckdb.sql(query).df()
+#     st.dataframe(result)
+#
+#     try:
+#         result = result[solution_df.columns]
+#         st.dataframe(result.compare(solution_df))
+#     except KeyError as e:
+#         st.write("There are some missing columns")
+#
+#     rows_difference = result.shape[0] - solution_df.shape[0]
+#     if rows_difference != 0:
+#         st.write(f"result has {rows_difference} rows different than solution")
+#
+#
+# tab1, tab2 = st.tabs(["tables", "solution"])
+#
+# with tab1:
+#     st.write("beverages")
+#     st.dataframe(beverages)
+#     st.write("foods")
+#     st.dataframe(food_items)
+#     st.write("expected")
+#     st.dataframe(solution_df)
+#
+# with tab2:
+#     st.write(ANSWER_STR)
