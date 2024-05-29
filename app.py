a4411de0d62d05a33ca1fc04a7757f05b2ab42d6
SaeifEdd <saeifchaabouni@gmail.com>
2024-05-29 07:45:55 +0100

    for recheck

--- app.py
+++ app.py
@@ -21,7 +21,12 @@ with st.sidebar:
     )
 
     st.write("You selected:", theme)
-    exercise = con.execute(f"SELECT * FROM memory_state where theme = '{theme}' ").df()
+    exercise = (
+        con.execute(f"SELECT * FROM memory_state where theme = '{theme}' ")
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index(drop=True)
+    )
     st.write(exercise)
 
     exercise_name = exercise.loc[0, "exercise name"]
@@ -51,7 +56,7 @@ if query:
 tab1, tab2 = st.tabs(["tables", "solution"])
 
 with tab1:
-    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    exercise_tables = exercise.loc[0, "tables"]
     for table in exercise_tables:
         st.write(f"table:{table}")
         table_df = con.execute(f"SELECT * FROM {table}").df()
